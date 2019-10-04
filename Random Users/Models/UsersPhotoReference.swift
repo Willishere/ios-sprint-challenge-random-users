c923bf033ddd8f3bae52d129c2553f353d3cf348
William Chen <chen.will7@gmail.com>
2019-10-04 08:44:48 -0700

    storyboard finished

---
+++ Random Users/Models/UsersPhotoReference.swift
@@ -0,0 +1,19 @@
+//
+//  MarsPhotoReference.swift
+//  Random Users
+//
+//  Created by William Chen on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct UsersPhotoReference: Codable {
+    var results: [String]
+    
+    ResultsCodingKey: String, CodingKey {
+        enum 
+    }
+    
+}
+ 
